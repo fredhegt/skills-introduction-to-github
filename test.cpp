7bcb1edb9612beb488eeec22e1c335f49d38165b
Freddie Noordhoek Hegt <FreddieHegt@MacBook-Pro-257.local>
2025-01-29 18:15:44 +0000

    Removed nested Git repository

---
+++ test.cpp
@@ -0,0 +1,11 @@
+// L1/simple.cpp
+// Another particularly simple example of C++ in action!
+// Niels Walet, last updated 22/01/2023
+#include <iostream>
+
+int main()
+{
+const int current_year = 2023;
+std::cout << "C++ is the best programming language in "<<current_year<<"!"<< std::endl;
+return 0;
+}
